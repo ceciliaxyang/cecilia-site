a2c1dff56c84e9951d6b2421a3a5001b66acf07f
cecilia <e.oxypetalum@gmail.com>
2013-11-17 16:41:22 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cysite::Application.config.secret_token = '99bf9a95b4f41022fe83c25dc39cfff26baaf11539994cdc868ce15c13689c8de01c7cf0fd9591c041af5db5ea4b945bfb5b3c60031667a6866e14ba95a0716a'
